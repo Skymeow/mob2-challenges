f37de674b73b385b29a69b90f232294b57ed6fd0
Sky Xu <skyrocks88xmz@gmail.com>
2017-10-16 10:24:56 -0700

    update ui

--- frontEndTrip/tripPlanner/tripPlanner/SignupViewController.swift
+++ frontEndTrip/tripPlanner/tripPlanner/SignupViewController.swift
@@ -12,7 +12,40 @@ class SignupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let width = 0.25*UIScreen.main.bounds.size.width
+        let height = 0.15*UIScreen.main.bounds.size.height
+        let width2 = 0.5*UIScreen.main.bounds.size.width
+        let nameLabel = UILabel(frame: CGRect(x: 20, y: 100, width: width, height: height))
+        self.view.addSubview(nameLabel)
+        nameLabel.text = "Username"
+        nameLabel.textAlignment = .left
+        
+        let emailLabel = UILabel(frame: CGRect(x: 20, y: 200, width: width, height: height))
+        self.view.addSubview(emailLabel)
+        emailLabel.text = "Email"
+        emailLabel.textAlignment = .left
+        
+        let passwordLabel = UILabel(frame: CGRect(x: 20, y: 300, width: width, height: height))
+        self.view.addSubview(passwordLabel)
+        passwordLabel.text = "Password"
+        passwordLabel.textAlignment = .left
+        
+        let nameInput = UITextField(frame: CGRect(x: 150, y: 100, width: width2, height: height))
+        self.view.addSubview(nameInput)
+        nameInput.placeholder = "type in your username"
+        
+        let emailInput = UITextField(frame: CGRect(x: 150, y: 200, width: width2, height: height))
+        self.view.addSubview(emailInput)
+        emailInput.placeholder = "type in your email"
+        
+        let passwordInput = UITextField(frame: CGRect(x: 150, y: 300, width: width2, height: height))
+        self.view.addSubview(passwordInput)
+        passwordInput.placeholder = "type in your password"
+        
+        let username = nameInput.text
+        let email = emailInput.text
+        let password = passwordInput.text
+        
     }
 
     override func didReceiveMemoryWarning() {
