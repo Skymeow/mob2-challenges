26f3bb51e772e9581af33ece147ff84e62b23bf6
Sky Xu <skyrocks88xmz@gmail.com>
2017-10-17 09:54:20 -0700

    add trips

--- frontEndTrip/tripPlanner/tripPlanner/Trip.swift
+++ frontEndTrip/tripPlanner/tripPlanner/Trip.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import UIKit
+
+struct Waypoint: Decodable {
+    let destination: String?
+    let lat: String?
+    let long: String?
+    
+    init(destination: String?, lat: String?, long: String?) {
+        self.destination = destination
+        self.lat = lat
+        self.long = long
+    }
+}
+struct Trip: Decodable {
+    let user_email: String?
+    let completed: Bool?
+    let destination: String?
+    let trip_name: String?
+    let start_time: String?
+    let waypoints: [Waypoint]
+    
+    init(user_email: String?, completed: Bool?, destination: String?, trip_name: String?, start_time: String?, waypoints: [Waypoint]? ) {
+        self.user_email = user_email
+        self.completed = completed
+        self.destination = destination
+        self.trip_name = trip_name
+        self.start_time = start_time
+        self.waypoints = waypoints!
+    }
+    
+}
+
+struct ArrayTrips: Decodable {
+    let trips: [Trip]
+}
